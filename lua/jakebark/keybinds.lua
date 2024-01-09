d7428d45afd37c002375726a8d312035e5de1e48
Jake <39654191+Barkest@users.noreply.github.com>
2024-01-09 07:00:46 +0000

    mn keybind

--- lua/jakebark/keybinds.lua
+++ lua/jakebark/keybinds.lua
@@ -4,6 +4,8 @@ vim.keymap.set("n", "<leader>pv", ":Vex<CR>")
 vim.keymap.set("n", "Z", vim.cmd.Ex)
 vim.keymap.set("n", "<leader><CR>", ":so ~/.config/nvim/init.lua<CR>")
 vim.keymap.set("n", "<leader>cv", ":e ~/.config/nvim/<CR>")
+vim.keymap.set("n", "<leader>mn", ":e ~/notes/<CR>")
+
 vim.keymap.set("n", "<leader>gf", ":GFiles<CR>")
 vim.keymap.set("n", "<leader>ff", ":Files<CR>")
 vim.keymap.set("n", "C-j", "cnext<CR")
