a62d65228e98b7dcc60cffd7464c9c9bd71d036b
Jake <39654191+Barkest@users.noreply.github.com>
2024-04-25 20:21:31 +0100

    highlight yank

--- lua/jakebark/settings.lua
+++ lua/jakebark/settings.lua
@@ -7,3 +7,20 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.scrolloff = 8
+
+
+local augroup = vim.api.nvim_create_augroup
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
