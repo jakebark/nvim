d966a08b2a595df460b84c02dce9c4564b3885c3
jakebark <jacob.barker@gmail.com>
2025-02-12 11:03:14 +0000

    grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,18 +2,17 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>gs', builtin.grep_string, {})
 
--- find files in nvim directory
 vim.keymap.set('n', '<leader>cv', function()
     builtin.find_files { cwd = vim.fn.stdpath 'config' }
-end, { desc = 'config > nvim' })
+end, { desc = 'config/nvim' })
 
--- find files in notes
 vim.keymap.set('n', '<leader>mn', function()
     builtin.find_files { cwd = "~/notes" }
 end, { desc = 'my notes' })
 
--- search buffer
 vim.keymap.set('n', '<leader>/', function()
     builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
         winblend = 10,
