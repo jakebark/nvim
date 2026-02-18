2205716fe770fcfac72ab305840d8bbc77cfaa1b
jakebark <jacob.barker@gmail.com>
2026-02-18 20:33:20 +0000

    update keybinds

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -3,16 +3,16 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":Vex<CR>")
 vim.keymap.set("n", "Z", vim.cmd.Ex)
 vim.keymap.set("n", "<leader><CR>", ":so ~/.config/nvim/init.lua<CR>")
-vim.keymap.set("n", "<leader>cv", ":e ~/.config/nvim/<CR>")
+--vim.keymap.set("n", "<leader>cv", ":e ~/.config/nvim/<CR>")
 vim.keymap.set("n", "<leader>n", ":Notes<CR>")
 
 
-vim.keymap.set("n", "<leader>gf", ":GFiles<CR>")
-vim.keymap.set("n", "<leader>ff", ":Files<CR>")
+--vim.keymap.set("n", "<leader>gf", ":GFiles<CR>")
+--vim.keymap.set("n", "<leader>ff", ":Files<CR>")
 vim.keymap.set("n", "<leader>pf", ":Vex<CR>:Files<CR>")
-vim.keymap.set("n", "C-j", "cnext<CR")
-vim.keymap.set("n", "C-k", "cprev<CR")
 
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 
 --vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 --vim.keymap.set("n", "<leader>Y", [["+Y]])
@@ -28,7 +28,3 @@ vim.keymap.set({ 'n', 'v' }, '<leader>D', '"+D') -- delete line
 -- Paste from system clipboard
 vim.keymap.set('n', '<leader>p', '"+p') -- paste after cursor
 vim.keymap.set('n', '<leader>P', '"+P') -- paste before cursor
-
--- grep
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
