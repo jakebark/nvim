5220d4488ca2a15d0892fba37bf2a7a96fd92615
jakebark <jacob.barker@gmail.com>
2026-02-18 19:38:21 +0000

    update treesitter settings

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,7 +2,10 @@ require("nvim-treesitter.configs").setup({
     ensure_installed = { "dockerfile", "go", "hcl", "json", "lua", "markdown", "python", "rust", "vim", "yaml" },
     sync_install = false,
     auto_install = true,
+    ignore_install = {},
+    modules = {},
     highlight = {
         enable = true,
+        additional_vim_regex_highlighting = false,
     },
 })
